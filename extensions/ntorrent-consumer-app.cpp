acee7eb1e88e224d148fc5cf296f7fcab3b80d89
Akshay <2210019+AkshayRaman@users.noreply.github.com>
2018-03-07 17:32:27 -0800

    Reduce simulation time for debugging

--- extensions/ntorrent-consumer-app.cpp
+++ extensions/ntorrent-consumer-app.cpp
@@ -58,7 +58,7 @@ NTorrentConsumerApp::StartApplication()
 {
     ndn::App::StartApplication();
     ndn::FibHelper::AddRoute(GetNode(), "/", m_face, 0);
-    for(int i=0;i<100;i++)
+    for(int i=0;i<5;i++)
         Simulator::Schedule(Seconds(i+1.0), &NTorrentConsumerApp::SendInterest, this);
 }
 
